5c0a221aabcfea6d3e64baeb83f5cb3bcc0c35f1
selva j <dhoniselva192003@gmail.com>
2025-01-04 22:03:40 +0530

    Time: 203 ms (51.57%), Space: 45.7 MB (30.89%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,21 +1,29 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        Set<String> uniquePalindromes = new HashSet<>();
+        int total = 0;
+        int[] firstIndex = new int[26];
+        int[] lastIndex = new int[26];
+        Arrays.fill(firstIndex, -1);
 
-        for (int i = 1; i < s.length() - 1; i++) {
-            Set<Character> leftChars = new HashSet<>();
-            for (int j = 0; j < i; j++) {
-                leftChars.add(s.charAt(j));
+        for(int i = 0; i < s.length(); i++) {
+            int ch = s.charAt(i) - 'a';
+            if(firstIndex[ch] == - 1) {
+                firstIndex[ch] = i;
             }
-
-            for (int k = i + 1; k < s.length(); k++) {
-                if (leftChars.contains(s.charAt(k))) {
-                    String palindrome = "" + s.charAt(k) + s.charAt(i) + s.charAt(k);
-                    uniquePalindromes.add(palindrome);
-                }
-            }
+            lastIndex[ch] = i;
         }
+        for(int i = 0; i < 26; i++) {
+            if(firstIndex[i] == -1) continue;
 
-        return uniquePalindromes.size();
+            int start = firstIndex[i];
+            int end = lastIndex[i];
+
+            Set<Character> set = new HashSet<>();
+            for(int j = start + 1; j < end; j++) {
+                set.add(s.charAt(j));
+            }
+            total += set.size();
+        }
+        return total;
     }
 }
