4ac848a42d031897a042444536cdf1d0d99f0ba1
selva j <dhoniselva192003@gmail.com>
2025-11-21 12:36:14 +0530

    Time: 31 ms (98.48%), Space: 47.1 MB (17.63%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,29 +1,38 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int total = 0;
-        int[] firstIndex = new int[26];
-        int[] lastIndex = new int[26];
-        Arrays.fill(firstIndex, -1);
+        int n = s.length();
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, n);
+        Arrays.fill(last, -1);
 
-        for(int i = 0; i < s.length(); i++) {
-            int ch = s.charAt(i) - 'a';
-            if(firstIndex[ch] == - 1) {
-                firstIndex[ch] = i;
-            }
-            lastIndex[ch] = i;
+        // find first & last
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            first[c] = Math.min(first[c], i);
+            last[c] = i;
         }
-        for(int i = 0; i < 26; i++) {
-            if(firstIndex[i] == -1) continue;
 
-            int start = firstIndex[i];
-            int end = lastIndex[i];
+        int res = 0;
 
-            Set<Character> set = new HashSet<>();
-            for(int j = start + 1; j < end; j++) {
-                set.add(s.charAt(j));
+        // for each character as the outer sides
+        for (int c = 0; c < 26; c++) {
+            int L = first[c];
+            int R = last[c];
+            if (R - L < 2) continue; // no space inside
+            
+            boolean[] memo = new boolean[26];
+
+            // scan only between L and R
+            for (int i = L + 1; i < R; i++) {
+                int idx = s.charAt(i) - 'a';
+                if (!memo[idx]) {
+                    memo[idx] = true;
+                    res++;
+                }
             }
-            total += set.size();
         }
-        return total;
+
+        return res;
     }
 }
